f0ccf585c0e01be479f4d99e630826ede6c25509
Takhir Nafikov <takhir.learn@gmail.com>
2019-07-26 21:22:36 +0300

    chapter9,  Workout begin

---
+++ chapter9/src/main/java/com/example/chapter9/WorkoutDetailFragment.java
@@ -0,0 +1,45 @@
+package com.example.chapter9;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class WorkoutDetailFragment extends Fragment {
+
+    private long workoutId;
+
+
+    public WorkoutDetailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        View view = getView();
+        if (view != null) {
+            TextView title = view.findViewById(R.id.textTitle);
+            Workout workout = Workout.workouts[(int) workoutId];
+            title.setText(workout.getName());
+            TextView description = view.findViewById(R.id.textDescription);
+            description.setText(workout.getDescription());
+        }
+    }
+
+    public void setWorkout(long id) {
+        this.workoutId = id;
+    }
+}
