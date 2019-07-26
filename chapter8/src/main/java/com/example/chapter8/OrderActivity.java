336ab824d478303bb83c3f7e55577a6b81aea0f3
Takhir Nafikov <takhir.learn@gmail.com>
2019-07-26 19:57:07 +0300

    chapter8,   Bits and Pizzas begin

---
+++ chapter8/src/main/java/com/example/chapter8/OrderActivity.java
@@ -0,0 +1,21 @@
+package com.example.chapter8;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.app.ActionBar;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+}
