9fd0dc04e2b0d0c875eb2faaa1332f07988c0868
Takhir Nafikov <takhir.learn@gmail.com>
2019-07-27 18:19:53 +0300

    chapter 9-10-11,  Workout finish

--- chapter9/src/main/java/com/example/chapter9/WorkoutDetailFragment.java
+++ chapter9/src/main/java/com/example/chapter9/WorkoutDetailFragment.java
@@ -3,6 +3,7 @@ package com.example.chapter9;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,7 +22,14 @@ public class WorkoutDetailFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        if (savedInstanceState != null) {
+        if (savedInstanceState == null) {
+            StopwatchFragment stopwatch = new StopwatchFragment();
+            FragmentTransaction ft = getChildFragmentManager().beginTransaction();
+            ft.add(R.id.stopwatch_container, stopwatch);
+            ft.addToBackStack(null);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.commit();
+        } else {
             workoutId = savedInstanceState.getLong("workoutId");
         }
     }
