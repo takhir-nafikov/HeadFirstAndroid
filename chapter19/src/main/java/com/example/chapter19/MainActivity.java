1d301a041d4b4db9ed8094c844be6c8ae3a5e7e5
Takhir Nafikov <takhir.learn@gmail.com>
2019-07-29 01:58:26 +0300

    chapter19, Odometer

---
+++ chapter19/src/main/java/com/example/chapter19/MainActivity.java
@@ -0,0 +1,135 @@
+package com.example.chapter19;
+
+import android.app.Activity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class MainActivity extends Activity {
+
+    private OdometerService odometer;
+    private boolean bound = false;
+    private final int PERMISSION_REQUEST_CODE = 698;
+    private final int NOTIFICATION_ID = 423;
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder binder) {
+            OdometerService.OdometerBinder odometerBinder =
+                    (OdometerService.OdometerBinder) binder;
+            odometer = odometerBinder.getOdometer();
+            bound = true;
+        }
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            bound = false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        displayDistance();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(this, OdometerService.class);
+                    bindService(intent, connection, Context.BIND_AUTO_CREATE);
+                } else {
+                    NotificationManager notificationManager =
+                            (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+                    String channelId = "some_channel_id";
+                    CharSequence channelName = "Some Channel";
+                    int importance = NotificationManager.IMPORTANCE_LOW;
+
+                    NotificationChannel notificationChannel =
+                            new NotificationChannel(channelId, channelName, importance);
+                    notificationChannel.setVibrationPattern(new long[]{1000, 1000});
+
+                    notificationManager.createNotificationChannel(notificationChannel);
+
+                    NotificationCompat.Builder builder2 =
+                            new NotificationCompat.Builder(this, channelId)
+                                    .setSmallIcon(android.R.drawable.sym_def_app_icon)
+                                    .setContentTitle(getResources().getString(R.string.app_name))
+                                    .setContentText(getResources().getString(R.string.permission_denied))
+                                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                                    .setAutoCancel(true);
+
+                    Intent actionIntent = new Intent(this, MainActivity.class);
+                    PendingIntent actionPendingIntent = PendingIntent.getActivity(this, 0,
+                            actionIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                    builder2.setContentIntent(actionPendingIntent);
+
+                    notificationManager.notify(NOTIFICATION_ID, builder2.build());
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (ContextCompat.checkSelfPermission(this, OdometerService.PERMISSION_STRING)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{OdometerService.PERMISSION_STRING},
+                    PERMISSION_REQUEST_CODE);
+        } else {
+            Intent intent = new Intent(this, OdometerService.class);
+            bindService(intent, connection, Context.BIND_AUTO_CREATE);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (bound) {
+            unbindService(connection);
+            bound = false;
+        }
+    }
+
+    private void displayDistance() {
+        final TextView distanceView = findViewById(R.id.distance);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                double distance = 0.0;
+                if (bound && odometer != null) {
+                    distance = odometer.getDistance();
+                }
+                String distanceStr = String.format(Locale.getDefault(),
+                        "%1$,.2f miles", distance);
+                distanceView.setText(distanceStr);
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
+}
