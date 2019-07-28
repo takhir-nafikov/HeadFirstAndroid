0db545d5440cd6f05fed6a109b0f375e4d339145
Takhir Nafikov <takhir.learn@gmail.com>
2019-07-28 19:35:22 +0300

    chapter 14: CatChat

---
+++ chapter14/src/main/java/com/example/chapter14/SentItemsFragment.java
@@ -0,0 +1,25 @@
+package com.example.chapter14;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SentItemsFragment extends Fragment {
+
+
+    public SentItemsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sent_items, container, false);
+    }
+
+}
---
+++ chapter14/src/main/java/com/example/chapter14/TrashFragment.java
@@ -0,0 +1,16 @@
+package com.example.chapter14;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TrashFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_trash, container, false);
+    }
+}
